bbeb69ea3a99d3ddb9ce91730d8a5f3a96a247de
jortizme <jortizmez@gmail.com>
2020-05-02 11:12:25 +0200

    a script to automatize the tests

--- run_bot.bash
+++ run_bot.bash
@@ -4,4 +4,64 @@
 # gelesene Daten werden in Verzeichnis 'download' geschrieben. 
 #
 
-./bot testSites_10_2019.txt --webreq-delay 100 --webreq-path download
+execute(){
+
+./bot $1 --webreq-delay 0 --webreq-path $2/$3_0ms $4
+./bot $1 --webreq-delay 300 --webreq-path $2/$3_300ms $4
+./bot $1 --webreq-delay 500 --webreq-path $2/$3_500ms $4
+./bot $1 --webreq-delay 1000 --webreq-path $2/$3_1000ms $4
+
+}
+
+TIMEFILE="elapsed_time.txt"
+DOWNLOADDIR="downloads
+
+echo " Please enter the File you want to read the internet sides from "
+
+read SITESFILE
+
+if [ ! -f $SITESFILE ]; then
+	
+	echo "$SITESFILE doesn't exists"
+	echo
+	exit
+fi
+
+if [ ! -f $TIMEFILE ]; then
+	
+	touch $TIMEFILE
+	echo
+else
+	echo "" > $TIMEFILE	
+fi
+
+if [ ! -d $DOWNLOADDIR ]; then
+
+	mkdir $DOWNLOADDIR
+else
+	cd $DOWNLOADDIR
+	rm -r *ms
+	cd ..
+fi
+
+echo "Please enter the number of times you want to run the test.."
+
+read CHANGENUMBER
+
+COUNTER=0
+
+while [ "$COUNTER" -ne "$CHANGENUMBER" ]; do
+	
+	echo	
+	echo "Please set the number of clients at the beginning of bot.cpp"
+      	echo "Compile the file and enter the amount of clients to run the test..."
+ 	read CLIENTAMOUNT
+	
+	echo "" >> $TIMEFILE
+	execute $SITESFILE $DOWNLOADDIR $CLIENTAMOUNT $TIMEFILE
+	((COUNTER++))	
+
+done	
+
+echo "Test finished..."
+exit
